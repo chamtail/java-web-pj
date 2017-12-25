1bc48c9d3e60beff69991ee6c70c9cd227345458
xiaowangzi <theo@Theos-MacBook-Pro.local>
2017-12-26 00:41:18 +0800

    waiting hint

--- axiba/src/Room.js
+++ axiba/src/Room.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import "./App.css";
 import Axios from "axios";
-import {Avatar, Button, Icon, Layout, Tag} from "antd";
+import {Avatar, Button, Icon, Layout, Modal, Spin, Tag} from "antd";
 
 const {Header, Content, Footer} = Layout;
 
@@ -81,12 +81,18 @@ class Room extends Component {
 
     render() {
         return (
-            <Layout>
-                <Header>
+            <div>
+                <Modal visible={this.state.currentPlayer === null || this.state.currentPlayer !== this.props.userName}
+                       closable={false} footer={null}>
+                    <Spin tip="Waiting for the opponent..."
+                          style={{display: "block"}}/>
+                </Modal>
+                <Layout>
+                    <Header>
                     <span>
                         <Tag color="geekblue">Room {this.props.roomName}</Tag>
                     </span>
-                    <span style={{float: "right"}}>
+                        <span style={{float: "right"}}>
                         <Avatar style={{backgroundColor: "red", verticalAlign: 'middle'}} size="large">
                             {this.state.player1Name === null ? "" : this.state.player1Name}
                         </Avatar>
@@ -96,79 +102,79 @@ class Room extends Component {
                             {this.state.player2Name === null ? "" : this.state.player2Name}
                         </Avatar>
                     </span>
-                </Header>
-                <Content>
-                    <div>
-                        {[0, 1, 2, 3].map((rowIndex) =>
-                            [0, 1, 2, 3].map((colIndex) =>
-                                <div className="game-dot"
-                                     style={{
-                                         top: (15 + rowIndex * 25) + "%",
-                                         left: (12 + colIndex * 25) + "%"
-                                     }}/>
-                            )
-                        )}
-                        {this.state.horizontal.map((row, rowIndex) =>
-                            row.map((stroke, colIndex) =>
-                                <div className={stroke ? "game-edge-horizontal-stuffed" : "game-edge-horizontal"}
-                                     onClick={() => {
-                                         let userName = this.props.userName;
-                                         let currentPlayer = this.state.currentPlayer;
-                                         if (userName === currentPlayer || currentPlayer === null) {
-                                             return;
-                                         }
-                                         Axios
-                                             .get(`/rooms/connect?roomName=${this.props.roomName}&userName=${userName}` +
-                                                 `&isHorizontal=true&row=${rowIndex}&col=${colIndex}`)
-                                             .then((ret) => {
-                                                 this.fetch();
-                                             });
-                                     }}
-                                     style={{
-                                         top: (15 + rowIndex * 25) + "%", left: (12 + colIndex * 25) + "%"
-                                     }}/>)
-                        )}
-                        {this.state.vertical.map((row, rowIndex) =>
-                            row.map((stroke, colIndex) =>
-                                <div className={stroke ? "game-edge-vertical-stuffed" : "game-edge-vertical"}
-                                     onClick={() => {
-                                         let userName = this.props.userName;
-                                         let currentPlayer = this.state.currentPlayer;
-                                         if (userName === currentPlayer || currentPlayer === null) {
-                                             return;
-                                         }
-                                         Axios
-                                             .get(`/rooms/connect?roomName=${this.props.roomName}&userName=${userName}` +
-                                                 `&isHorizontal=false&row=${rowIndex}&col=${colIndex}`)
-                                             .then((ret) => {
-                                                 this.fetch();
-                                             });
-                                     }}
-                                     style={{
-                                         left: (12 + colIndex * 25) + "%", top: (15 + rowIndex * 25) + "%"
-                                     }}/>)
-                        )}
-                        {this.state.square.map((row, rowIndex) =>
-                            row.map((fill, colIndex) =>
-                                <div className="game-edge-square"
-                                     style={{
-                                         left: (12 + colIndex * 25) + "%",
-                                         top: (15 + rowIndex * 25) + "%",
-                                         background: fill === 0 ? "white" : (fill === 1 ? "red" : "yellow")
-                                     }}/>)
-                        )}
-                    </div>
-                </Content>
-                <Footer>
-                    <Button type="primary" onClick={this.handleLeave}>
-                        <Icon type="left"/>Leave
-                    </Button>
-                    <Button type="primary" onClick={this.handleStart}>
-                        Start<Icon type="caret-right"/>
-                    </Button>
-                </Footer>
-            </Layout>
-
+                    </Header>
+                    <Content>
+                        <div>
+                            {[0, 1, 2, 3].map((rowIndex) =>
+                                [0, 1, 2, 3].map((colIndex) =>
+                                    <div className="game-dot"
+                                         style={{
+                                             top: (15 + rowIndex * 25) + "%",
+                                             left: (12 + colIndex * 25) + "%"
+                                         }}/>
+                                )
+                            )}
+                            {this.state.horizontal.map((row, rowIndex) =>
+                                row.map((stroke, colIndex) =>
+                                    <div className={stroke ? "game-edge-horizontal-stuffed" : "game-edge-horizontal"}
+                                         onClick={() => {
+                                             let userName = this.props.userName;
+                                             let currentPlayer = this.state.currentPlayer;
+                                             if (userName === currentPlayer || currentPlayer === null) {
+                                                 return;
+                                             }
+                                             Axios
+                                                 .get(`/rooms/connect?roomName=${this.props.roomName}&userName=${userName}` +
+                                                     `&isHorizontal=true&row=${rowIndex}&col=${colIndex}`)
+                                                 .then((ret) => {
+                                                     this.fetch();
+                                                 });
+                                         }}
+                                         style={{
+                                             top: (15 + rowIndex * 25) + "%", left: (12 + colIndex * 25) + "%"
+                                         }}/>)
+                            )}
+                            {this.state.vertical.map((row, rowIndex) =>
+                                row.map((stroke, colIndex) =>
+                                    <div className={stroke ? "game-edge-vertical-stuffed" : "game-edge-vertical"}
+                                         onClick={() => {
+                                             let userName = this.props.userName;
+                                             let currentPlayer = this.state.currentPlayer;
+                                             if (userName === currentPlayer || currentPlayer === null) {
+                                                 return;
+                                             }
+                                             Axios
+                                                 .get(`/rooms/connect?roomName=${this.props.roomName}&userName=${userName}` +
+                                                     `&isHorizontal=false&row=${rowIndex}&col=${colIndex}`)
+                                                 .then((ret) => {
+                                                     this.fetch();
+                                                 });
+                                         }}
+                                         style={{
+                                             left: (12 + colIndex * 25) + "%", top: (15 + rowIndex * 25) + "%"
+                                         }}/>)
+                            )}
+                            {this.state.square.map((row, rowIndex) =>
+                                row.map((fill, colIndex) =>
+                                    <div className="game-edge-square"
+                                         style={{
+                                             left: (12 + colIndex * 25) + "%",
+                                             top: (15 + rowIndex * 25) + "%",
+                                             background: fill === 0 ? "white" : (fill === 1 ? "red" : "yellow")
+                                         }}/>)
+                            )}
+                        </div>
+                    </Content>
+                    <Footer>
+                        <Button type="primary" onClick={this.handleLeave}>
+                            <Icon type="left"/>Leave
+                        </Button>
+                        <Button type="primary" onClick={this.handleStart}>
+                            Start<Icon type="caret-right"/>
+                        </Button>
+                    </Footer>
+                </Layout>
+            </div>
         );
     }
 }
